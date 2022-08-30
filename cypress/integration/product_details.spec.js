e7f01b04b2b9e38d56dc4aa4278582040129c747
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-30 22:32:23 +0000

    add testing for product_details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,15 @@
+
+describe('product page', () => {
+  before(() => {
+    cy.visit("/")
+  })
+
+  it('displays the product page', () => {
+    cy.get(".products article")
+      .first()
+      .click()
+    
+    cy.get('.page-header h1').should('be.visible')
+    cy.get('.quantity span').first().should('have.text', '18 in stock at ')
+  })
+})
